7e5bf98e54a870f139f3182f8f48e7d919cfb592
yogyy <yogimr261@gmail.com>
2026-01-17 22:19:37 +0700

    Initialize Vite PWA

---
+++ src/lib/PWABadge.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+  import { useRegisterSW } from 'virtual:pwa-register/svelte'
+
+  // periodic sync is disabled, change the value to enable it, the period is in milliseconds
+  // You can remove onRegisteredSW callback and registerPeriodicSync function
+  const period = 0
+
+  /**
+   * This function will register a periodic sync check every hour, you can modify the interval as needed.
+   */
+  function registerPeriodicSync(swUrl: string, r: ServiceWorkerRegistration) {
+    if (period <= 0) return
+
+    setInterval(async () => {
+      if ('onLine' in navigator && !navigator.onLine)
+        return
+
+      const resp = await fetch(swUrl, {
+        cache: 'no-store',
+        headers: {
+          'cache': 'no-store',
+          'cache-control': 'no-cache',
+        },
+      })
+
+      if (resp?.status === 200)
+        await r.update()
+    }, period)
+  }
+
+  const { needRefresh, updateServiceWorker } = useRegisterSW({
+    onRegisteredSW(swUrl, r) {
+      if (period <= 0) return
+      if (r?.active?.state === 'activated') {
+        registerPeriodicSync(swUrl, r)
+      }
+      else if (r?.installing) {
+        r.installing.addEventListener('statechange', (e) => {
+          const sw = e.target as ServiceWorker
+          if (sw.state === 'activated')
+            registerPeriodicSync(swUrl, r)
+        })
+      }
+    },
+  })
+
+  function close() {
+      
+      needRefresh.set(false)
+  }
+
+  let toast = $derived($needRefresh)
+  let message = $derived($needRefresh ? 'New content available, click on reload button to update.' : '')
+</script>
+
+{#if toast}
+  <div
+    class="pwa-toast"
+    role="alert"
+    aria-labelledby="toast-message"
+  >
+    <div class="message">
+      <span id="toast-message">
+        { message }
+      </span>
+    </div>
+    <div class="buttons">
+      {#if $needRefresh}
+        <button type="button" onclick={() => updateServiceWorker(true)}>
+          Reload
+        </button>
+      {/if}
+      <button type="button" onclick={close}>
+        Close
+      </button>
+    </div>
+  </div>
+{/if}
+
+<style>
+  .pwa-toast {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    margin: 16px;
+    padding: 12px;
+    border: 1px solid #8885;
+    border-radius: 4px;
+    z-index: 2;
+    text-align: left;
+    box-shadow: 3px 4px 5px 0 #8885;
+    background-color: white;
+  }
+  .pwa-toast .message {
+    margin-bottom: 8px;
+  }
+  .pwa-toast .buttons {
+    display: flex;
+  }
+  .pwa-toast button {
+    border: 1px solid #8885;
+    outline: none;
+    margin-right: 5px;
+    border-radius: 2px;
+    padding: 3px 10px;
+  }
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'my-pengeluaran',
+      short_name: 'my-pengeluaran',
+      description: 'pengeluaran gweh',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
