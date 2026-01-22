105c9004fe3d26b782851dc97d6435badb450f09
yogyy <yogimr261@gmail.com>
2026-01-22 23:41:31 +0700

    feat: add tailwind & shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,43 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  plugins: [
+    tailwindcss(),
+    viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
+    svelte(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'my-pengeluaran',
-      short_name: 'my-pengeluaran',
-      description: 'pengeluaran gweh',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: "myPengeluaran",
+        short_name: "myPengeluaran",
+        description: "pengeluaran gweh",
+        theme_color: "#ffffff",
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
