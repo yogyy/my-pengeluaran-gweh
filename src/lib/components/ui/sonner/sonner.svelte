d98617b3372a2d4501ba029f1740cea6cbbd16c1
yogyy <yogimr261@gmail.com>
2026-02-11 22:57:16 +0700

    feat: add context menu and sonner

--- src/lib/components/ui/sonner/sonner.svelte
+++ src/lib/components/ui/sonner/sonner.svelte
@@ -8,10 +8,13 @@
     Toaster as Sonner,
     type ToasterProps as SonnerProps,
   } from "svelte-sonner";
+  import { mode } from "mode-watcher";
+
   let { ...restProps }: SonnerProps = $props();
 </script>
 
 <Sonner
+  theme={mode.current}
   class="toaster group"
   style="--normal-bg: var(--color-popover); --normal-text: var(--color-popover-foreground); --normal-border: var(--color-border);"
   {...restProps}
