0a48bf96c178e1e3a95adfb405d47f77cc50f34f
yogyy <yogimr261@gmail.com>
2026-02-20 21:16:07 +0700

    feat: add menu component for theme, language, and currency selection

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
   import SummaryCards from "$lib/components/summary-cards.svelte";
-  import TransactionForm from "$lib/components/transaction-form.svelte";
   import TransactionList from "$lib/components/transaction-list.svelte";
   import PWABadge from "$lib/components/PWABadge.svelte";
   import { Toaster } from "$lib/components/ui/sonner";
   import ChartPieDonutText from "$lib/components/chart-pie-donut-text.svelte";
   import { ModeWatcher } from "mode-watcher";
-  import DarkModeToggle from "$lib/components/dark-mode-toggle.svelte";
+  import { m } from "./paraglide/messages.js";
+  import Menu from "$lib/components/menu.svelte";
 </script>
 
 <main class="bg-background text-foreground min-h-screen p-4 md:p-8">
   <div class="mx-auto max-w-4xl">
-    <!-- Header -->
-    <div class="mb-8">
-      <h1 class="mb-2 text-4xl font-bold">Expense Tracker</h1>
-      <p class="text-muted-foreground">
-        Manage your income and expenses efficiently
-      </p>
+    <div class="relative mb-8 flex justify-between">
+      <div class="">
+        <h1 class="mb-2 text-4xl font-bold">{m.title()}</h1>
+        <p class="text-muted-foreground">
+          {m.description()}
+        </p>
+      </div>
+      <Menu />
     </div>
     <!-- Summary Cards -->
     <SummaryCards />
